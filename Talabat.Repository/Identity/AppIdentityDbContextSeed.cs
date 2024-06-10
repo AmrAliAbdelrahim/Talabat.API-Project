f34af29d44642c162c83abc95a989210c477793c
Mohamed Elhaddad <melhdadd@hotmail.com>
2024-06-10 21:02:29 +0300

    Add project files.

---
+++ Talabat.Repository/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Repository.Identity
+{
+    public static class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> _userManager)
+        {
+            if(_userManager.Users.Count() == 0)
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Amr Ali",
+                    Email = "amr607048@gmail.com",
+                    UserName = "Amr.Ali",
+                    PhoneNumber = "01122334455"
+                };
+
+                await _userManager.CreateAsync(user , "Pa$$w0rd");
+            }
+        }
+    }
+}
